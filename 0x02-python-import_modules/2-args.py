156543b9419e53b562e0f8adcc4c4256508abaac
youssefeemad <eyoussef228@gmail.com>
2023-11-30 20:13:34 +0200

    new

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    
+
 argc = len(argv)
 
 if argc < 2:
@@ -13,4 +13,3 @@ else:
         print("{} arguments:".format(argc - 1))
     for n in range(1, argc):
         print("{}: {}".format(n, argv[n]))
-        
