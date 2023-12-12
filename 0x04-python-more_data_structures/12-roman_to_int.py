c6a0dee9de11fe6f815b1b3ae3b5a141ce78b1e7
youssefeemad <eyoussef228@gmail.com>
2023-12-12 18:34:26 +0200

    new

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,34 @@
 #!/usr/bin/python3
+def roman_value(prmCharacter):
+    roman_list = [('I', 1), ('V', 5), ('X', 10),
+                  ('L', 50), ('C', 100), ('D', 500), ('M', 1000)]
+    for item in roman_list:
+        character, value = item
+        if (prmCharacter is character):
+            return value
+    return None
+
+
+def next_value(prmString, prmIndex):
+    if prmIndex + 1 < len(prmString):
+        return roman_value(prmString[prmIndex + 1])
+    else:
+        return None
+
+
 def roman_to_int(roman_string):
-	if roman_string is None or type(roman_string) is not str:
-		return 0
-	roman_numeral = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-					 'C': 100, 'D': 500, 'M': 1000}
-	num = 0
-	for i in range(len(roman_string)):
-		if i > 0 and roman_numeral[roman_string[i]] > roman_numeral[roman_string[i - 1]]:
-			num += roman_numeral[roman_string[i]] - 2 * roman_numeral[roman_string[i - 1]]
-		else:
-			num += roman_numeral[roman_string[i]]
-	return num
+    result = 0
+
+    if (roman_string is None or isinstance(roman_string, str) is False):
+        return result
+
+    enum = enumerate(roman_string)
+    for index, character in enum:
+        currentValue = roman_value(character)
+        nextValue = next_value(roman_string, index)
+        if nextValue is None or currentValue >= nextValue:
+            result += currentValue
+        else:
+            result += (nextValue - currentValue)
+            next(enum)
+    return result
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-	return sum(set(my_list))
+    new_list = set(my_list)
+    sum = 0
+    for i in new_list:
+        sum += i
+    return sum
