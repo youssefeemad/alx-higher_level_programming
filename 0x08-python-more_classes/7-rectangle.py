0ad4e19d2b54737aaffde8741001da89a8895ec5
youssefeemad <eyoussef228@gmail.com>
2024-01-04 18:51:06 +0200

    new

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -8,6 +8,7 @@ class Rectangle:
     """Representation of a rectangle"""
 
     number_of_instances = 0
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """Initializes the rectangle"""
@@ -62,7 +63,7 @@ class Rectangle:
         """returns printable string representation of the rectangle"""
         string = ""
         if self.__width != 0 and self.__height != 0:
-            string += "\n".join("#" * self.__width
+            string += "\n".join(str(self.print_symbol) * self.__width
                                 for j in range(self.__height))
         return string
 
